21b5949ff26509b45fc3abc00d82acd65c0112bc
dchang74 <NELSONC924@GMAIL.COM>
2023-04-29 18:49:27 -0400

    added the open/close functionality for filters and finished the range picker for the price filter

--- app/components/PriceRangeBar.jsx
+++ app/components/PriceRangeBar.jsx
@@ -1,115 +1,118 @@
 'use client'
-import React from 'react'
+import React, { useCallback, useEffect, useState, useRef  } from 'react'
 import Link from 'next/link'
 import '../styles/priceRangeBar.css'
+import PropTypes from "prop-types";
 
+const PriceRangeBar = ({ min, max, onChange }) => {
+  const [minVal, setMinVal] = useState(min);
+  const [maxVal, setMaxVal] = useState(max);
+  const minValRef = useRef(min);
+  const maxValRef = useRef(max);
+  const range = useRef(null);
 
-const PriceRangeBar = () => {
-    function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
-        const [from, to] = getParsed(fromInput, toInput);
-        fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
-        if (from > to) {
-            fromSlider.value = to;
-            fromInput.value = to;
-        } else {
-            fromSlider.value = from;
-        }
+  // Convert to percentage
+  const getPercent = useCallback(
+    (value) => Math.round(((value - min) / (max - min)) * 100),
+    [min, max]
+  );
+
+  // Set width of the range to decrease from the left side
+  useEffect(() => {
+    const minPercent = getPercent(minVal);
+    const maxPercent = getPercent(maxValRef.current);
+
+    if (range.current) {
+      range.current.style.left = `${minPercent}%`;
+      range.current.style.width = `${maxPercent - minPercent}%`;
     }
-        
-    function controlToInput(toSlider, fromInput, toInput, controlSlider) {
-        const [from, to] = getParsed(fromInput, toInput);
-        fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
-        setToggleAccessible(toInput);
-        if (from <= to) {
-            toSlider.value = to;
-            toInput.value = to;
-        } else {
-            toInput.value = from;
-        }
+  }, [minVal, getPercent]);
+
+  // Set width of the range to decrease from the right side
+  useEffect(() => {
+    const minPercent = getPercent(minValRef.current);
+    const maxPercent = getPercent(maxVal);
+
+    if (range.current) {
+      range.current.style.width = `${maxPercent - minPercent}%`;
     }
-    
-    function controlFromSlider(fromSlider, toSlider, fromInput) {
-      const [from, to] = getParsed(fromSlider, toSlider);
-      fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
-      if (from > to) {
-        fromSlider.value = to;
-        fromInput.value = to;
-      } else {
-        fromInput.value = from;
-      }
-    }
-    
-    function controlToSlider(fromSlider, toSlider, toInput) {
-      const [from, to] = getParsed(fromSlider, toSlider);
-      fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
-      setToggleAccessible(toSlider);
-      if (from <= to) {
-        toSlider.value = to;
-        toInput.value = to;
-      } else {
-        toInput.value = from;
-        toSlider.value = from;
-      }
-    }
-    
-    function getParsed(currentFrom, currentTo) {
-      const from = parseInt(currentFrom.value, 10);
-      const to = parseInt(currentTo.value, 10);
-      return [from, to];
-    }
-    
-    function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-        const rangeDistance = to.max-to.min;
-        const fromPosition = from.value - to.min;
-        const toPosition = to.value - to.min;
-        controlSlider.style.background = `linear-gradient(
-          to right,
-          ${sliderColor} 0%,
-          ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
-          ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
-          ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
-          ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
-          ${sliderColor} 100%)`;
-    }
-    
-    function setToggleAccessible(currentTarget) {
-      const toSlider = document.querySelector('#toSlider');
-      if (Number(currentTarget.value) <= 0 ) {
-        toSlider.style.zIndex = 2;
-      } else {
-        toSlider.style.zIndex = 0;
-      }
-    }
-    
-    const fromSlider = document.querySelector('#fromSlider');
-    const toSlider = document.querySelector('#toSlider');
-    const fromInput = document.querySelector('#fromInput');
-    const toInput = document.querySelector('#toInput');
-    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
-    setToggleAccessible(toSlider);
-    
-    fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
-    toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
-    fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
-    toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+  }, [maxVal, getPercent]);
+
+  // Get min and max values when their state changes
+//   useEffect(() => {
+//     console.log("min:", minVal, " max:", maxVal);
+//   }, [minVal, maxVal, onChange]);
+
   return (
-    <div class="range_container">
-        <div class="sliders_control">
-            <input id="fromSlider" type="range" value="10" min="0" max="100"/>
-            <input id="toSlider" type="range" value="40" min="0" max="100"/>
-        </div>
-        <div class="form_control">
-            <div class="form_control_container">
-                <div class="form_control_container__time">Min</div>
-                <input class="form_control_container__time__input" type="number" id="fromInput" value="10" min="0" max="100"/>
-            </div>
-            <div class="form_control_container">
-                <div class="form_control_container__time">Max</div>
-                <input class="form_control_container__time__input" type="number" id="toInput" value="40" min="0" max="100"/>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div className="container">
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={minVal}
+        onChange={(event) => {
+          const value = Math.min(Number(event.target.value), maxVal);
+          setMinVal(value);
+          minValRef.current = value;
+        }}
+        className="thumb thumb--left"
+        style={{ zIndex: minVal > max - 100 && "5" }}
+      />
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={maxVal}
+        onChange={(event) => {
+          const value = Math.max(Number(event.target.value), minVal);
+          setMaxVal(value);
+          maxValRef.current = value;
+        }}
+        className="thumb thumb--right"
+      />
 
-export default PriceRangeBar
+      <div className="slider">
+        <div className="slider-track" />
+        <div ref={range} className="slider-range" />
+      </div>
+      <div className="values">
+        <div className="values-min-input">
+            <input 
+                type="number" 
+                min={minVal} 
+                max={maxVal}
+                value={minVal} 
+                onChange={(event) => {
+                    const value = Math.min(Number(event.target.value), maxVal);
+                    setMinVal(value);
+                    minValRef.current = value;
+                }}
+            />
+        </div>
+        <div className="values-max-input"> 
+            <input 
+                type="number"
+                min={minVal} 
+                max={maxVal}
+                value={maxVal} 
+                onChange={(event) => {
+                    const value = Math.max(Number(event.target.value), minVal);
+                    setMaxVal(value);
+                    maxValRef.current = value;
+                }}
+            />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+PriceRangeBar.propTypes = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default PriceRangeBar;
+
+
--- app/women/page.jsx
+++ app/women/page.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+'use client'
+import React, { useContext, useEffect, useState } from 'react'
 import PriceRangeBar from '../components/PriceRangeBar'
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 
 const page = () => {
+    const [openActiveFilters, setOpenActiveFilters] = useState(false)
+    const [openDesignerFilters, setOpenDesignerFilters] = useState(false)
+    const [openCategoryFilters, setOpenCategoryFilters] = useState(false)
+    const [openSizeFilters, setOpenSizeFilters] = useState(false)
+    const [openColorFilters, setOpenColorFilters] = useState(false)
+    const [openPriceFilters, setOpenPriceFilters] = useState(false)
+    console.log('active', openActiveFilters, 'designer', openDesignerFilters, 'category', openCategoryFilters, 'size',openSizeFilters, 'color', openColorFilters, 'price',openPriceFilters)
+    const openCloseFilters = (filter) => {
+        let currentFilter = filter.toLowerCase()
+        let checkFilter = null
+        if(filter === 'designer'){
+            checkFilter = openDesignerFilters
+        }else if(filter === 'category'){
+            checkFilter = openCategoryFilters
+        }else if(filter === 'size'){
+            checkFilter = openSizeFilters
+        }else if(filter === 'color'){
+            checkFilter = openColorFilters
+        }else{checkFilter = openPriceFilters}
+        console.log(`open${filter}Filters`, )
+        if (checkFilter) {
+            console.log()
+            document.querySelector(`.${currentFilter}-filters-body`).classList.add('open-filters')
+            document.querySelector(`.${currentFilter}-plus`).classList.add('close-filters')
+            document.querySelector(`.${currentFilter}-minus`).classList.add('open-filters')
+        }else {
+            document.querySelector(`.${currentFilter}-filters-body`).classList.remove('open-filters')
+            document.querySelector(`.${currentFilter}-plus`).classList.remove('close-filters')
+            document.querySelector(`.${currentFilter}-minus`).classList.remove('open-filters')
+        }
+    }
+    useEffect(() => {
+        // openCloseFilters('Active')
+        openCloseFilters('designer')
+        openCloseFilters('category')
+        openCloseFilters('size')
+        openCloseFilters('color')
+        openCloseFilters('price')
+
+    }, [openActiveFilters, openCategoryFilters, openColorFilters, openDesignerFilters, openSizeFilters, openPriceFilters])
+
   return (
     <div className='woman-page'>
         <div className="woman-page-header">
@@ -22,7 +65,14 @@ const page = () => {
                 <div className="designer-filters">
                     <div className="designer-filters-header">
                         <p>DESIGNERS</p>
-                        <p>-/+</p>
+                        <p 
+                            className='designer-plus'
+                            onClick={() => setOpenDesignerFilters(!openDesignerFilters)}
+                        ><AiOutlinePlus/></p>
+                        <p 
+                            className='designer-minus'
+                            onClick={() => setOpenDesignerFilters(!openDesignerFilters)}
+                        ><AiOutlineMinus/></p>
                     </div>
                     <div className="designer-filters-body">
                         <div className="designer-filter-search">
@@ -36,8 +86,15 @@ const page = () => {
                 </div>
                 <div className="category-filters">
                     <div className="category-filters-header">
-                        <p>DESIGNERS</p>
-                        <p>-/+</p>
+                        <p>CATEGORY</p>
+                        <p 
+                            className='category-plus'
+                            onClick={() => setOpenCategoryFilters(!openCategoryFilters)}
+                        ><AiOutlinePlus/></p>
+                        <p 
+                            className='category-minus'
+                            onClick={() => setOpenCategoryFilters(!openCategoryFilters)}
+                        ><AiOutlineMinus/></p>
                     </div>
                     <div className="category-filters-body">
                         {/* loop over all categories and return a component 
@@ -52,7 +109,14 @@ const page = () => {
                 <div className="size-filters">
                     <div className="size-filters-header">
                         <p>SIZES</p>
-                        <p>-/+</p>
+                        <p 
+                            className='size-plus'
+                            onClick={() => setOpenSizeFilters(!openSizeFilters)}
+                        ><AiOutlinePlus/></p>
+                        <p 
+                            className='size-minus'
+                            onClick={() => setOpenSizeFilters(!openSizeFilters)}
+                        ><AiOutlineMinus/></p>
                     </div>
                     <div className="size-filters-body">
                         {/* All sizes offered updated by filters  */}
@@ -66,7 +130,14 @@ const page = () => {
                 <div className="color-filters">
                     <div className="color-filters-header">
                         <p>COLORS</p>
-                        <p>-/+</p>
+                        <p 
+                            className='color-plus'
+                            onClick={() => setOpenColorFilters(!openColorFilters)}
+                        ><AiOutlinePlus/></p>
+                        <p 
+                            className='color-minus'
+                            onClick={() => setOpenColorFilters(!openColorFilters)}
+                        ><AiOutlineMinus/></p>
                     </div>
                     <div className="color-filters-body">
                         {/* All colors offered*/}
@@ -80,11 +151,22 @@ const page = () => {
                 <div className="price-filters">
                     <div className="price-filters-header">
                         <p>PRICE</p>
-                        <p>-/+</p>
+                        <p 
+                            className='price-plus'
+                            onClick={() => setOpenPriceFilters(!openPriceFilters)}
+                        ><AiOutlinePlus/></p>
+                        <p 
+                            className='price-minus'
+                            onClick={() => setOpenPriceFilters(!openPriceFilters)}
+                        ><AiOutlineMinus/></p>
                     </div>
                     <div className="price-filters-body">
                         {/* range slider componet to range a min and max price to filter */}
-                        <PriceRangeBar/>
+                        <PriceRangeBar
+                            min={0}
+                            max={80000}
+                            onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
+                        />
                     </div>
                 </div>
                         
